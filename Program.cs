a83657b0120d7f645ca6d98e0d0ea607cf78f439
Sergei21D <113889182+Sergei21D@users.noreply.github.com>
2022-10-31 11:56:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleApplication7
+{
+    public enum DayOfWeek
+    {
+        Понедельник = 0,
+        Вторник = 1,
+        Среда = 2,
+        Четверг = 3,
+        Пятниццо = 4,
+        Суббота = 5,
+        Воскресенье = 6
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("n=");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine((DayOfWeek)((n - 1) % Enum.GetValues(typeof(DayOfWeek)).Length));
+        }
+    }
+}
